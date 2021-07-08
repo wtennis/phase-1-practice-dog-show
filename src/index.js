b0a286c346abeb8991c8fc985945936110c4ef3f
Whiting <whitinghtennis@gmail.com>
2021-07-08 10:13:35 -0700

    commit tree 1cdbf0dc6746a12cf3585928b2ab8ae5ed1cbec2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+fetchDogs()
 })
+
+function fetchDogs() {
+    fetch('http://localhost:3000/dogs')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(addDog)
+    });
+};
+
+function addDog(dog) {
+    let tr = document.createElement('tr')
+    let tdName = document.createElement('td')
+    tdName.textContent = `${dog["name"]}`
+    let tdBreed = document.createElement('td')
+    tdBreed.textContent = `${dog["breed"]}`
+    let tdSex = document.createElement('td')
+    tdSex.textContent = `${dog["sex"]}`
+    let tdButton = document.createElement('td')
+    let button = document.createElement('button')
+    button.textContent = `Edit Dog`
+    tdButton.append(button)
+
+    let dogTable = document.querySelector('#table-body')
+
+    tr.append(tdName, tdBreed, tdSex, tdButton)
+    dogTable.append(tr)
+
+    button.addEventListener('click', () => {
+        document.getElementsByName('name')[0]['value']= `${dog["name"]}`
+        document.getElementsByName('breed')[0]['value']= `${dog["breed"]}`
+        document.getElementsByName('sex')[0]['value']= `${dog["sex"]}`
+    }
+    )
+
+    let dogForm = document.querySelector("dog-form")
+    dogForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        fetch(`http://localhost:3000/dogs/${dog['id']}`)
+        //***This is where I stopped*** I haven't learned patch yet. 
+        // After this dogForm event listener is working (clicking 'Submit' patches the API),
+        //next steps might be to delete all dogs from table and reinvoke fetchDogs
+    })
+
+
+}
+
+/*
+// I wonder if figuring out this way would yield cleaner code:
+function dogInnerHTML(dog) {
+    // return (`<tr><td>${dog["name"]}</td> <td>${dog["breed"]}</td> <td>${dog["sex"]}</td> <td><button>Edit</button></td></tr>
+   // `)
+}
+*/
+
